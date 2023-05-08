e9915d365ad0646884a99b6ea9defbf5aea589c7
Prince Jimmy <101404861+Braintech22@users.noreply.github.com>
2023-05-08 11:47:34 +0100

    new add

--- src/components/CardGrid.js
+++ src/components/CardGrid.js
@@ -4,29 +4,32 @@ import Cards from './Cards';
 const CardGrid = () => {
   const cardData = [
     {
-      imageSrc: "https://images.pexels.com/photos/247791/pexels-photo-247791.png?auto=compress&cs=tinysrgb&w=600",
-      title: "Frontend Development",
-      content: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+      imageSrc: "https://images.unsplash.com/photo-1633356122544-f134324a6cee?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmVhY3QlMjBqc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60",
+      title: "software Development",
+      content: "Html5/Css3/Javascript, React, Bootstrap, typescript, node.js ES6, Python",
       buttonText: "Go somewhere",
     },
+
+    {
+      imageSrc: "https://images.pexels.com/photos/196644/pexels-photo-196644.jpeg?auto=compress&cs=tinysrgb&w=600",
+      title: "UI/UX",
+      content: "UI/UX design and wireframing, siteMap, Prototyping and Animations ",
+      buttonText: "Go somewhere",
+    },
+
     {
         imageSrc: "https://images.pexels.com/photos/1624895/pexels-photo-1624895.jpeg?auto=compress&cs=tinysrgb&w=600",
-        title: "Cloud Technology",
-        content: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+        title: "Cloud Computing",
+        content: "Azure cloud Administration, Data security, Application deployment, networking and cloud storage ",
         buttonText: "Go somewhere",
       },
       {
         imageSrc: "https://images.pexels.com/photos/3183153/pexels-photo-3183153.jpeg?auto=compress&cs=tinysrgb&w=600",
-        title: "Project Management",
-        content: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+        title: "Project Management/Devops",
+        content: "Jira, GitHub, CI/CD, Agile Scrum, Google analytics, Saleforce administration, Risk mgt",
         buttonText: "Go somewhere",
       },
-      {
-        imageSrc: "https://images.pexels.com/photos/196644/pexels-photo-196644.jpeg?auto=compress&cs=tinysrgb&w=600",
-        title: "UI/UX",
-        content: "Some quick example text to build on the card title and make up the bulk of the card's content.",
-        buttonText: "Go somewhere",
-      },
+      
     // Add more card objects as needed
   ];
 
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <footer className=" downpart mt-auto py-3 " style={{position: 'fixed', bottom: 0, left: 0, width: '100%', zIndex: 1}}>
       <Container className="text-center">
-        <p>&copy; {new Date().getFullYear()} My Website Name</p>
+        <p>&copy; {new Date().getFullYear()} prince jimmy</p>
       </Container>
     </footer>
   );
